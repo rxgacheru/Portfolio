2dced9ed64501676a84abbf6b18c5644e1f9aed4
rxgacheru <roygacheru01@gmail.com>
2024-05-13 13:15:18 +0300

    add

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff0000',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
